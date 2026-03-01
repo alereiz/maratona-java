50e175d871f4ab16d46a96df6f8a9760c9381db4
Alexandre Reis <alereis7e@gmail.com>
2026-03-01 18:23:07 -0300

    Uso do String, e nível da linguagem modificada para o Java 8

--- src/academydevdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academydevdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,9 +4,12 @@ public class Aula02TiposPrimitivos {
     public static void main (String[] args) {
 
         int age = 23;
-        System.out.println("Tenho "+age+" anos.\n");
+            System.out.println("Tenho "+age+" anos.\n");
 
         int castingIntToLong = (int) 10000000000L;
-        System.out.println(castingIntToLong);
+            System.out.println(castingIntToLong+"\n");
+
+        String nome = "Alexandre G. d. Reis";
+            System.out.println("Oi, me chamo "+nome);
     }
 }
