5c55ab8ab34ca2a474953dcf9b714371958fb5d5
Alexandre Reis <alereis7e@gmail.com>
2026-03-01 15:02:07 -0300

    Tipo int e concatenação de strings com variáveis

---
+++ src/academydevdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,10 @@
+package academydevdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main (String[] args) {
+
+        int age = 23;
+
+        System.out.println("Tenho "+age+" anos.");
+    }
+}
