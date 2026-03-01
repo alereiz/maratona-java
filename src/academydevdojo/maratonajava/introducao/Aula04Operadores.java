342ae556d726208fd0c6acb781d724625bb2bc11
Alexandre Reis <alereis7e@gmail.com>
2026-03-01 19:36:26 -0300

    Operadores Aritméticos

---
+++ src/academydevdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,25 @@
+package academydevdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    // + - * /
+    public static void main (String[] args){
+
+        double num1 = 7;
+        double num2 = 23;
+
+        double resSum = num1 + num2;
+        double resSub = num1 - num2;
+        double resMul = num1 * num2;
+        double resDiv = num1 / num2;
+
+        System.out.println(resSum);
+        System.out.println(resSub);
+        System.out.println(resMul);
+        System.out.println(resDiv);
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+    }
+}
