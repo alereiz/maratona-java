4dbba93eb3de2000f08345d431e663db11feed44
Alexandre Reis <alereis7e@gmail.com>
2026-03-01 14:31:18 -0300

    Dividindo classes por aula para me localizar

--- src/academydevdojo/maratonajava/introducao/Aula01OlaAle.java
+++ src/academydevdojo/maratonajava/introducao/Aula01OlaAle.java
@@ -1,6 +1,6 @@
 package academydevdojo.maratonajava.introducao;
 
-public class OlaAle {
+public class Aula01OlaAle {
     public static void main (String[] args) {
         System.out.println("KA-ME-HA-ME-HA");
     }
