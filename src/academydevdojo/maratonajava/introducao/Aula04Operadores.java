21644d3f79bfd610f4a34a8c8bdbc5e4ec634bec
Alexandre Reis <alereis7e@gmail.com>
2026-03-02 11:56:50 -0300

    Funcionamento dos operadores de atribuição

--- src/academydevdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academydevdojo/maratonajava/introducao/Aula04Operadores.java
@@ -27,15 +27,28 @@ public class Aula04Operadores {
 
         //Ops. Relacionais: < > <= >= == !=
         boolean is2MaiorQue3  = 2 > 3;
-            System.out.println("2 é maior que 3? "+is2MaiorQue3);
+        System.out.println("2 é maior que 3? "+is2MaiorQue3);
 
         boolean is2MenorQue3  = 2 < 3;
-            System.out.println("2 é menor que 3? "+is2MenorQue3);
+        System.out.println("2 é menor que 3? "+is2MenorQue3);
 
         boolean is2Igual3     = 2 == 3;
-            System.out.println("2 é igual a 3? "+is2Igual3);
+        System.out.println("2 é igual a 3? "+is2Igual3);
 
         boolean is2Diferente3 = 2 != 3;
-            System.out.println("2 é diferente de 3? "+is2Diferente3);
+        System.out.println("2 é diferente de 3? "+is2Diferente3);
+
+        //Ops. Atribuição: = += -= *= /= %=
+        double bonus = 1000;
+        bonus += 800; // eq. à bonus = bonus + 800;
+        System.out.println(bonus);
+
+        //Incremento e Decremento: ++ --
+        //No início:
+        int iI = 0;
+        System.out.println("Incremento no início: "+ ++iI);
+        //No final:
+        int iF = 0;
+        System.out.println("Incremento no final: "+iF++);
     }
 }
