b8474ca44031845cc23a5603337256a5a714f436
Alexandre Reis <alereis7e@gmail.com>
2026-03-03 13:56:24 -0300

    Uso básico do IF e atribuição dá lógica à uma variável booleana ao invés do IF diretamente

---
+++ src/academydevdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,20 @@
+package academydevdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main (String[] args){
+
+        if(false){
+            System.out.println("Dentro do IF");
+        }
+        System.out.println("Fora do IF");
+
+        // Resultado de condicionais dentro de variáveis booleanas:
+        int age = 23;
+        boolean isAutorizado = age >= 18;
+        if(isAutorizado){
+            System.out.println("Autorizado a comprar bebida alcoólica.");
+        } else {
+            System.out.println("Não autorizado a comprar bebida alcoólica.");
+        }
+    }
+}
