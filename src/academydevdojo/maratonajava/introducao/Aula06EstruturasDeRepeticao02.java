0961b64c7d0090e3a27643d0332220144a68d743
Alexandre Reis <alereis7e@gmail.com>
2026-03-05 09:53:05 -0300

    Exercício prático do uso do for

---
+++ src/academydevdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao02.java
@@ -0,0 +1,16 @@
+package academydevdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao02 {
+    public static void main (String[] args) {
+
+        /*
+        * Exercício:
+        *
+        * Imprima todos os números pares de ZERO até UM MILHÃO.
+        * */
+
+        for (int i = 0; i <= 1000000; i += 2){
+            System.out.println(i);
+        }
+    }
+}
